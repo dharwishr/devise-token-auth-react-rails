3e879420ea4f827fd10778618eb1f6153e2004bb
Dharwish Raj <drpmiro@gmail.com>
2022-11-16 00:58:41 +0530

    Initial commit

---
+++ app/controllers/users/registration_controller.rb
@@ -0,0 +1,8 @@
+class Users::RegistrationController  < DeviseTokenAuth::RegistrationsController
+    
+    private
+
+        def sign_up_params
+            params.permit(:email, :password, :password_confirmation, :name)
+        end
+end
